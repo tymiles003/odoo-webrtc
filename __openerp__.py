a37d8ec8cc1966d823da80af3299447a41cd0059
Wagner Pereira <wagner.pereira@kmee.com.br>
2016-10-23 08:23:46 -0200

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,9 @@
+{
+  "name": "Koble Odoo WebRTC base files",
+  "version": "8.0.0",
+  "description": "Basic WebRTC files to be used by feature modules.",
+  "author": "Koble Open Solutions (Wagner Pereira)",
+  "depends": ['web'],
+  "installable": True,
+  "auto_install": False
+}
